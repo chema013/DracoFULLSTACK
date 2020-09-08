710614cf148c232e981b8ebf95113d5cbbb9dcd3
Chema <chema_013@hotmail.com>
2020-09-08 15:42:21 -0500

    Prueba heroku

--- app/config/config.js
+++ app/config/config.js
@@ -9,7 +9,7 @@ let urlDB;
 
 if (process.env.NODE_ENV === 'dev') {
     // urlDB = 'mongodb://localhost:27017/draco';
-    urlDB = 'mongodb+srv://chemadb:Mechemix.1@cluster0.xxx6p.azure.mongodb.net/chemadb?retryWrites=true&w=majority';
+    urlDB = 'mongodb+srv://chemadb:Mechemix.1@cluster0.xxx6p.azure.mongodb.net/draco?retryWrites=true&w=majority';
 } else {
     // urlDB = process.env.MONGO_URI;
     urlDB = 'mongodb+srv://chemadb:Mechemix.1@cluster0.xxx6p.azure.mongodb.net/draco?retryWrites=true&w=majority';
