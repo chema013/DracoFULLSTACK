3ffd3d28d7defa23ac179b2f23984791f9bfd60c
Chema <chema_013@hotmail.com>
2020-09-08 15:26:21 -0500

    second commit

--- app/config/config.js
+++ app/config/config.js
@@ -8,9 +8,11 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 let urlDB;
 
 if (process.env.NODE_ENV === 'dev') {
-    urlDB = 'mongodb://localhost:27017/draco';
+    // urlDB = 'mongodb://localhost:27017/draco';
+    urlDB = 'mongodb+srv://chemadb:Mechemix.1@cluster0.xxx6p.azure.mongodb.net/chemadb?retryWrites=true&w=majority';
 } else {
-    urlDB = process.env.MONGO_URI;
+    // urlDB = process.env.MONGO_URI;
+    urlDB = 'mongodb+srv://chemadb:Mechemix.1@cluster0.xxx6p.azure.mongodb.net/draco?retryWrites=true&w=majority';
 }
 process.env.URLDB = urlDB;
 
